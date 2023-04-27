1d824d293893358bfcbdd5dee4dd0c4c121310a1
Albert Montufar <amontufar@madcapsoftware.com>
2023-04-27 11:44:12 -0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,9 @@
+#Given a string only containing round brackets ' ( ) ' and lowercase characters,
+# remove the least amount of brackets so the string is valid
+# A string is considered valid if it's empty or if all the brackets close
+
+# Sample input "a)bc(d)" Removing the one closing bracket creates a valid string
+#"))((" we have to remove all brackets (emtpy or all brackets should close
+
+
+
